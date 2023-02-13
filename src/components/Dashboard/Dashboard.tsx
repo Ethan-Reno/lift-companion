881cc547e2b633faf0ca656060371bebc7c1f7aa
Ethan Reno <epreno@live.ca>
2023-02-12 23:59:45 -0500

    Remove temporary API call in dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,22 +1,14 @@
 import { type User } from 'next-auth';
-import React, { useState } from 'react';
+import React from 'react';
 import { PlusIcon } from '@radix-ui/react-icons';
 import Dialog, { DIALOG_TYPES } from '../shared/Dialog/Dialog';
 import Button from '../shared/Button/Button';
-import { api } from "../../utils/api";
 
 interface DashboardProps {
   user: User;
 }
 
 const Dashboard = ({ user }: DashboardProps) => {
-  const utils = api.useContext();
-  const [ name, setName ] = useState('');
-  const [ primaryUnit, setPrimaryUnit ] = useState('');
-  const [ secondaryUnit, setSecondaryUnit ] = useState('');
-
-  const postExericse = api.exercise.postExercise.useMutation();
-
   const triggerButton = (
     <Button>
       <PlusIcon className="h-6 w-6 pr-2" />
