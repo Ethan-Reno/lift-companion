65be95cdeee63f99afce28f0a2227a11737a1fe0
Ethan Reno <epreno@live.ca>
2023-05-27 21:11:54 -0400

    Update CreateExerciseModal to use new composable approach

--- src/components/Modals/CreateExerciseModal.tsx
+++ src/components/Modals/CreateExerciseModal.tsx
@@ -1,29 +1,37 @@
 import React, { useState } from 'react';
-import { type SubmitHandler, useForm } from "react-hook-form";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Form,
+  FormControl,
+  FormField,
+  FormLabel,
+  FormItem,
+  FormMessage,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from 'lift-companion-ui'; 
+import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
-import { z, type TypeOf } from 'zod';
+import { z } from 'zod';
 import { api } from '../../utils/api';
-import { Button } from 'lift-companion-ui'
-import { FormInput } from '../shared/FormInput/FormInput';
 import { Loader2 } from "lucide-react"
-import { Dialog } from 'lift-companion-ui';
 
-export const createExerciseSchema = z.object({
-  name: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name must be a string",
-  }).min(1),
-  primaryUnit: z.string({
-    required_error: "primaryUnit is required",
-    invalid_type_error: "primaryUnit must be a string",
-  }).min(1),
-  secondaryUnit: z.string({
-    required_error: "secondaryUnit is required",
-    invalid_type_error: "secondaryUnit must be a string",
-  }).min(1)
-})
-
-export type CreateExerciseInputs = TypeOf<typeof createExerciseSchema>;
+const createExerciseSchema = z.object({
+  name: z.string().min(1, { message: 'Name is required' }),
+  description: z.string().min(1, { message: 'Description is required' }),
+  //meausrement enum is required
+  measurements: z.enum(['WeightRep', 'DistanceRep', 'TimeRep']),
+  primaryUnit: z.enum(['POUND', 'KILOGRAM', 'METER', 'MILE', 'KILOMETER', 'YARD', 'SECOND', 'MINUTE', 'HOUR']),
+});
 
 export const CreateExerciseModal = () => {
   const [ isOpen, setIsOpen ] = useState(false);
@@ -32,30 +40,106 @@ export const CreateExerciseModal = () => {
     onMutate: () => console.log('mutating'),
     onSettled: () => {setIsOpen(false)},
   });
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<CreateExerciseInputs>(
-    {
+  
+  const CreateExerciseForm = () => {
+    const form = useForm<z.infer<typeof createExerciseSchema>>({
       resolver: zodResolver(createExerciseSchema),
+      defaultValues: {
+        name: "",
+        description: "",
+        measurements: "WeightRep",
+        primaryUnit: "POUND",
+      },
+    });
+    
+    const onSubmit = (values: z.infer<typeof createExerciseSchema>) => {
+      createExercise.mutate(values);
     }
-  );
-
-  const onSubmit: SubmitHandler<CreateExerciseInputs> = data => 
-    createExercise.mutate(data);
-
-  const form = (
-    <div className="">
-      <form className="flex flex-col gap-10 pt-6" onSubmit={handleSubmit(onSubmit)}>
-        <FormInput register={register} field='name' label='Name' error={errors.name?.message} />
-        <FormInput register={register} field='primaryUnit' label='Primary Unit' error={errors.primaryUnit?.message} />
-        <FormInput register={register} field='secondaryUnit' label='Secondary Unit' error={errors.secondaryUnit?.message} />
-        <div className="mt-4 flex justify-end">
+  
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="Description" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="measurements"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Measurement</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue="WeightRep">
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pick a measurement" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="WeightRep">Weight x Reps</SelectItem>
+                    <SelectItem value="DistanceRep">Distance x Reps</SelectItem>
+                    <SelectItem value="TimeRep">Time x Reps</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* TODO: Refactor to display only units for selected measurement */}
+          <FormField
+            control={form.control}
+            name="primaryUnit"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Primary Unit</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue="POUND">
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Pick a unit" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="POUND">Pound</SelectItem>
+                    <SelectItem value="KILOGRAM">Kilogram</SelectItem>
+                    <SelectItem value="METER">Meter</SelectItem>
+                    <SelectItem value="MILE">Mile</SelectItem>
+                    <SelectItem value="KILOMETER">Kilometer</SelectItem>
+                    <SelectItem value="YARD">Yard</SelectItem>
+                    <SelectItem value="SECOND">Second</SelectItem>
+                    <SelectItem value="MINUTE">Minute</SelectItem>
+                    <SelectItem value="HOUR">Hour</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <Button
             type='submit'
             disabled={createExercise.isLoading}
+            className='justify-end'
           >
             {createExercise.isLoading ? (
               <>
@@ -66,18 +150,24 @@ export const CreateExerciseModal = () => {
               <>Create</>
             )}
           </Button>
-        </div>
-      </form>
-    </div>
-  );
+        </form>
+      </Form>
+    );
+  };
 
   return (
-    <Dialog
-      trigger={<Button>Create Exercise</Button>}
-      content={form}
-      title='Create new exercise'
-      isOpen={isOpen}
-      setIsOpen={setIsOpen}
-    />
-  )
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button onClick={() => setIsOpen(true)}>Create Exercise</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Exercise</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <CreateExerciseForm />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
